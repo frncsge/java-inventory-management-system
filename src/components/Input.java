3b44e3c1e1bf021c24b8af33a692d45dbd69b6bd
Francis Ge <francisge527@gmail.com>
2025-11-07 23:25:04 +0800

    show input fields

--- src/components/Input.java
+++ src/components/Input.java
@@ -4,18 +4,16 @@ import javax.swing.*;
 
 public class Input extends JPanel {
 
-    private JLabel label;
-    private JTextField textField;
-    private int textFieldCols = 10; /* column/width for the text field */
+    private final JTextField textField;
 
     public Input(String inputLabel) {
         //align the label and text field vertically
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        label = new JLabel(inputLabel);
+        JLabel label = new JLabel(inputLabel);
         label.setAlignmentX(LEFT_ALIGNMENT); //put label on left side
 
-        textField = new JTextField(textFieldCols);
+        textField = new JTextField(15);
         textField.setAlignmentX(LEFT_ALIGNMENT); //also put text field on left side so they align
 
         //add to the input panel
@@ -24,7 +22,7 @@ public class Input extends JPanel {
     }
 
     public String getInput() {
-        return textField.getText();
+        return textField.getText().trim();
     }
 
     public void setInput(String input) {
--- src/pages/InventoryPage.java
+++ src/pages/InventoryPage.java
@@ -12,17 +12,19 @@ public class InventoryPage extends BasePage {
     public void setUI() {
         setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("Inventory", SwingConstants.CENTER);
+        //title label
+        JLabel titleLabel = new JLabel("Inventory", SwingConstants.CENTER);
+        add(titleLabel, BorderLayout.NORTH);
 
-        add(label, BorderLayout.NORTH);
+        //add the panel containing all inputs
         add(inputs(), BorderLayout.WEST);
     }
 
+    //creates a panel containing all inputs and the add button
     private JPanel inputs() {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        //add padding
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 15, 15, 15));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 15, 15, 15)); //add padding
 
         nameInput = new Input("Item name");
         priceInput = new Input("Price");
@@ -30,12 +32,42 @@ public class InventoryPage extends BasePage {
         categoryInput = new Input("Category");
 
         Input[] allInputs = {nameInput, priceInput, qtyInput, categoryInput};
-
         for (Input input : allInputs) {
             input.setMaximumSize(input.getPreferredSize()); //maintains the default size of the inputs
-            panel.add(input);
+            panel.add(input); //add input to the panel
         }
 
+        //add the "add" button to the panel
+        JButton button = new JButton("Add");
+        button.addActionListener(e -> addItemToInventory()); //calls addItemToInventory method
+        panel.add(button);
+
         return panel;
+    }
+
+    //extracts value/text from the inputs
+    private Object[] getInputValue() {
+        String name = nameInput.getInput();
+        String price = priceInput.getInput();
+        String qty = qtyInput.getInput().trim();
+        String category = categoryInput.getInput();
+
+        return new Object[]{name, price, qty, category};
+    }
+
+    //clears the value of all inputs
+    private void clearInputValue() {
+        Input[] allInputs = {nameInput, priceInput, qtyInput, categoryInput};
+
+        for (Input input : allInputs) {
+            input.setInput("");
+        }
+    }
+
+    private void addItemToInventory() {
+        Object[] inputValues = getInputValue();
+        for (Object value : inputValues) System.out.println(value);
+
+        clearInputValue();
     }
 }
