0eb4b73a8e7c831d5861b72957926562343f286b
Francis Ge <francisge527@gmail.com>
2025-11-07 17:06:16 +0800

    add helper methods

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -9,30 +9,34 @@ public class MainFrame extends JFrame {
     private JPanel mainPanel = new JPanel(cardLayout);
 
     public MainFrame() {
-        //set up main frame
-        setTitle("Inventory Management System");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
+        setUpMainFrame();
 
-        //create sidebar and pass down an action listener
-        SidebarPanel sidebar = new SidebarPanel(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //show page based on the set action command. ex: Home, Inventory, Settings
-                cardLayout.show(mainPanel, e.getActionCommand());
-            }
+        //create sidebar and pass down an action event
+        SidebarPanel sidebar = new SidebarPanel(e -> {
+            //show page based on the set action command. ex: Home, Inventory, Settings
+            cardLayout.show(mainPanel, e.getActionCommand());
         });
 
         //add sidebar to the main frame
         add(sidebar, BorderLayout.WEST);
 
-        //add pages in the mainPanel
-        mainPanel.add(new HomePage(), "Home");
-        mainPanel.add(new InventoryPage(), "Inventory");
-        mainPanel.add(new SettingsPage(), "Settings");
+        setUpPages();
 
         //add main panel to the main frame
         add(mainPanel, BorderLayout.CENTER);
+    }
+
+    public void setUpMainFrame() {
+        setTitle("Inventory Management System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+    }
+
+    public void setUpPages() {
+        //add sidebar to the main frame
+        mainPanel.add(new HomePage(), "Home");
+        mainPanel.add(new InventoryPage(), "Inventory");
+        mainPanel.add(new SettingsPage(), "Settings");
     }
 
     public static void main(String[] args) {
